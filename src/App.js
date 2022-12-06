0c39cb73b97ac8aa877efdea747224a968332fa0
rishuari14 <rishikeshkeshari6@gmail.com>
2022-12-05 16:26:23 -0800

    git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ProfileCard from "./components/ProfileCard";
+import "./App.css";
+import { useEffect} from 'react'
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import loader from "./images/loader.gif";
+import { GET_USER_URL } from "./utility/url";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user,setUser] = useState([]);
+	const [loading,setLoading] = useState(false)
+
+    //SETTING UP API CALL
+
+		let config = {
+			method : 'get',
+			url: GET_USER_URL,
+		
+		  };
+
+// API CALL USING ASYNC AWAIT AND AXIOS
+
+		const getRowdata = async () => {
+		  try {
+			const response = await axios(config);
+			console.log(response.data.results)
+			setUser(response.data.results[0]);
+		   
+		  }
+		   catch (err) {
+			setUser(null)
+			
+		  } finally {
+			setLoading(false);
+		  }
+	
+		};
+
+   // Initial API CALL
+      
+	  useEffect(() => {
+
+		getRowdata();
+
+	  },  [ ])
+
+	  
+
+
+
+
+
+
+	return (
+		(loading || user == null ) ? <div className="background">
+		<div className="bg-pattern-top"></div>
+		<div className="bg-pattern-bottom"></div>
+
+	     <img className="loader" src={loader} alt="Loading" />
+	 <Button variant="contained" disabled={true}>Wait..</Button>
+	</div> : 
+
+		<div className="background">
+			<div className="bg-pattern-top"></div>
+			<div className="bg-pattern-bottom"></div>
+
+			<ProfileCard
+				name= {user.name && user.name.first ? user.name.first +' '+ user.name.last  :"Not Available"}
+				age= {user.dob && user.dob.age ? user.dob.age  :"Not Available"}
+				email={user.location && user.location.city ? user.location.city  :"Not Available"}
+				city={user.location && user.location.city ? user.location.city  :"Not Available"}
+				country={user.location && user.location.country ? user.location.country  :"Not Available"}
+				postcode={user.location && user.location.postcode ? user.location.postcode  :"Not Available"}
+			></ProfileCard>
+		 <Button variant="contained" disabled ={loading ? true : false} onClick={()=> {getRowdata();setLoading(true)} } >Next User</Button>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/ProfileCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./ProfileCard.css";
+import avatar from "../images/image-rita.png";
+
+function ProfileCard(props) {
+	return (
+		<div className="card-container">
+			<header>
+				<img src={avatar} alt={props.name} />
+			</header>
+			<h1 className="bold-text">
+				{props.name} <span className="normal-text">{props.age}</span>
+			</h1>
+			<h2 className="normal-text">{props.email}</h2>
+			<div className="social-container">
+				<div className="followers">
+					<h1 className="bold-text">{props.city}</h1>
+					<h2 className="smaller-text">City</h2>
+				</div>
+				<div className="likes">
+					<h1 className="bold-text">{props.country}</h1>
+					<h2 className="smaller-text">Country</h2>
+				</div>
+				<div className="photos">
+					<h1 className="bold-text">{props.postcode}</h1>
+					<h2 className="smaller-text">Postcode</h2>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ProfileCard;
